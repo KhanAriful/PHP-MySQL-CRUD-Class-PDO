ea46254ed9567efd0d21f14dbf5735708b2560fc
Ariful <ank4219@gmail.com>
2020-03-23 14:54:00 +0100

    Database Config File

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Database Connection Parameters
+ */
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_NAME', 'testDB');
+define('DB_CHARSET', 'utf8mb4');
