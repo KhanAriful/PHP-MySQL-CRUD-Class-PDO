0f901c426adc216479dce9187037057c7b2d3ffa
Ariful <ank4219@gmail.com>
2020-03-23 14:53:22 +0100

    Upgrade

--- SQL.php
+++ SQL.php
@@ -1,29 +1,47 @@
 <?php
+ /**
+  *	PHP MySQL CRUD Class PDO
+  *
+  * @author 	Ariful Khan <khan.ariful@hotmail.com>
+  * @copyright 	Copyright (c) 2020 Ariful Khan
+  * @license	https://github.com/KhanAriful/PHP-MySQL-CRUD-Class-PDO/blob/master/LICENSE MIT License
+  * @link 		https://khanariful.github.io/PHP-MySQL-CRUD-Class-PDO
+  */
+require 'config.php';
 class SQL {
-	protected $hostname;
-	protected $username;
-	protected $password;
-	protected $dbname;
-	protected $charset;
+	protected $hostname = DB_HOST;
+	protected $username = DB_USER;
+	protected $password = DB_PASSWORD;
+	protected $dbname = DB_NAME;
+	protected $charset = DB_CHARSET;
 	protected $pdo;
-	public function __construct($hostname, $username, $password, $dbname, $charset){
-		$this->hostname = $hostname;
-		$this->username = $username;
-		$this->password = $password;
-		$this->dbname = $dbname;
-		$this->charset = $charset;
+	/**
+	 * Default constructor
+	 *
+	 * Triggers the connect function by default
+	 */
+	public function __construct(){
 		$this->connect();
 	}
+	/**
+	 * @return Connection using the pdo extension
+	 */
   	public function connect(){
 		try{
 		    $this->pdo = new PDO("mysql:host=" . $this->hostname . ";dbname=" . $this->dbname . ";charset=" . $this->charset, $this->username, $this->password);
-		    /* Set the PDO error mode to exception */
+		    /**
+		     * Set the PDO error mode to exception
+		     */
 		    $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		} catch(PDOException $e){
 		    die("ERROR: Could not connect. " . $e->getMessage());
 		}
 		return $this->pdo;
 	}
+	/**
+	 * @param  Sql query
+	 * @return Data from MySQL DB
+	 */
 	public function getData($sql){
 		$rows = array();
 	    try{
@@ -41,6 +59,9 @@ class SQL {
 	    }
 	    return $rows;
 	}
+	/**
+	 * @param  Sql query
+	 */
 	public function execute($sql){
 	    try{
 	        $this->pdo->exec($sql);
@@ -48,6 +69,11 @@ class SQL {
 	        die("ERROR: Could not able to execute $sql. " . $e->getMessage());
 	    }
 	}
+	/**
+	 * Default destructor
+	 *
+	 * Closes the DB connection
+	 */
 	public function __destruct(){
 		unset($this->pdo);
 	}
